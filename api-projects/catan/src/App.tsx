3692feae204317bfbb95f95d7c8c6421241af3fe
lukastaniic <lukastannic@gmail.com>
2025-10-23 05:39:07 +0200

    catan - Initial pages structure for Home and Game

--- api-projects/catan/src/App.tsx
+++ api-projects/catan/src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Game from './pages/Game';
+
 function App() {
-  return <>Catan</>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/game' element={<Game />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
